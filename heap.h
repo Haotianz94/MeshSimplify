77783a54dc01edfffe99e874a4427ad7ba8854bd
sola777 <zhanghaotian1994@163.com>
2016-08-19 22:19:17 -0400

    mesh simplify

---
+++ heap.h
@@ -0,0 +1,52 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include"common.h"
+
+class BinaryHeap//双端优先队列 
+{
+private:
+    int _key[1000000]; 
+    int _pos[1000000]; 
+	double _error[1000000];
+	int _n;
+
+	bool cmp(int i,int j)
+	{
+		return (_error[_key[j]]<=_error[_key[i]]);
+	} 
+    void percolateup(int i);//上滤 
+    void percolatedown(int i);//下滤 
+    void swap_(int i,int j);//交换 
+    int properParent(int i);//此节点和两个子节点中更适合做父节点的值 
+    int parent(int i)//返回父节点 
+    {
+        return (i-1)>>1;
+    }
+    int lChild(int i)//返回左儿子 
+    {
+        i=((i<<1)+1);
+        return (i<_n)? i:-1;
+    }
+    int rChild(int i)//返回右儿子 
+    {
+        i=((i+1)<<1);
+        return (i<_n)? i:-1;
+    }
+    bool parentVaild(int i)//是否还有父节点 
+    {
+        return (i>0)&&(i<_n);
+    }
+public:
+    BinaryHeap():_n(0)
+	{
+		for( int i = 0; i < 1000000 ;i++)
+			_pos[i] = -1;
+	}
+    int pop();
+	void push( int id ,double error);
+	void change( int id ,double error);
+	void heaplify();
+};
+
+#endif
---
+++ object.h
@@ -0,0 +1,52 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include"common.h"
+#include"heap.h"
+
+class Object
+{
+private:
+	std::vector<Vertex*> _vertex;
+	std::vector<Edge*> _edge;
+	int* _npos;
+	int* _nnext;
+	int _nhead;
+	int* _tpos;
+	int* _tnext;
+	int _thead;
+	Triangle* _triangle;
+	BinaryHeap* _heap;
+	int _V0;
+	int _V;
+	double ratio;
+	int _tri;
+public:
+	Object() 
+	{
+		_heap = new BinaryHeap();
+		_triangle = new Triangle[500000];
+		_tri = 0;
+		_npos = new int[10000000];
+		_nnext = new int[10000000];
+		_tpos = new int[10000000];
+		_tnext = new int[10000000];
+		_nhead = 0;
+		_thead = 0;
+	}
+	~Object()
+	{
+		delete[] _triangle;
+		delete[] _npos;
+		delete[] _nnext;
+		delete[] _tpos;
+		delete[] _tnext;
+	}
+	void setRatio( double r) { ratio = r; }
+	void loadObj(char* obj);
+	void simplify();
+	void outputObj(char* obj);
+	void addNeighbor(Vertex* v, int eid);
+	void addTri(Vertex* v, int tid);
+};
+#endif
